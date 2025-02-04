6433f8759a93810b95aaec920b245d8c972d3a68
micael1ma <micael1ma.professional@gmail.com>
2025-02-04 14:21:50 -0300

    Find by id

--- src/main/java/com/micael1ma/desafio03/services/ClientService.java
+++ src/main/java/com/micael1ma/desafio03/services/ClientService.java
@@ -1,10 +1,21 @@
 package com.micael1ma.desafio03.services;
 
+import com.micael1ma.desafio03.dto.ClientDTO;
+import com.micael1ma.desafio03.entities.Client;
 import com.micael1ma.desafio03.repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ClientService {
 
+    @Autowired
     private ClientRepository repository;
+
+    @Transactional(readOnly = true)
+    public ClientDTO findById(Long id) {
+        Client client = repository.findById(id).get();
+        return new ClientDTO(client);
+    }
 }
