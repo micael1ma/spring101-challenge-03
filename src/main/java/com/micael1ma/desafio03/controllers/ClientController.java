b95e582609c97f2ac0c3b13f6672810d3680d832
micael1ma <micael1ma.professional@gmail.com>
2025-02-04 14:28:55 -0300

    Find all

--- src/main/java/com/micael1ma/desafio03/controllers/ClientController.java
+++ src/main/java/com/micael1ma/desafio03/controllers/ClientController.java
@@ -3,7 +3,10 @@ package com.micael1ma.desafio03.controllers;
 import com.micael1ma.desafio03.dto.ClientDTO;
 import com.micael1ma.desafio03.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +23,11 @@ public class ClientController {
     public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
         ClientDTO dto = service.findById(id);
         return ResponseEntity.ok(dto);
+    }
 
+    @GetMapping
+    public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable) {
+        Page<ClientDTO> dto = service.findAll(pageable);
+        return ResponseEntity.ok(dto);
     }
 }
