2e5287e430305490b0833c968869f01044389389
micael1ma <micael1ma.professional@gmail.com>
2025-02-09 03:31:40 -0300

    Controller handler

---
+++ src/main/java/com/micael1ma/desafio03/controllers/handlers/ControllerExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.micael1ma.desafio03.controllers.handlers;
+
+import com.micael1ma.desafio03.dto.CustomError;
+import com.micael1ma.desafio03.services.execptions.DatabaseException;
+import com.micael1ma.desafio03.services.execptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<CustomError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        CustomError error = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(error);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<CustomError> resourceNotFound(DatabaseException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        CustomError error = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(error);
+    }
+}
